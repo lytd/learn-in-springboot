2a7fe15c60604172bbecf0acacca5c684c38e35a
madi <madi901114@gmail.com>
2019-04-21 23:44:25 +0800

    requestmapping部分--未完

--- springbootweb/web-mvc/src/main/java/td/learn/springboot/controller/MappingDemoController.java
+++ springbootweb/web-mvc/src/main/java/td/learn/springboot/controller/MappingDemoController.java
@@ -1,28 +1,24 @@
 package td.learn.springboot.controller;
 
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.HttpRequest;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
 import td.learn.springboot.model.User;
 
 @Controller
-@RequestMapping("/demo1")
-public class SpringMVC {
-
-
+@RequestMapping("/requestMapping")
+public class MappingDemoController {
 
     @Autowired
     private User user;
 
-    @RequestMapping("/user")
-    @ResponseBody
-    public User getUser(){
 
+   // @RequestMapping(method = HttpMethod.GET)
+    public User getUser1(){
         return user;
+
     }
-
-
-
-
 }
